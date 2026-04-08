55bb5c3758a382b4edf3aaecfb1a453af06e8b77
Saif Ur Rehman Siddiqui <urrehmansaif406@gmail.com>
2026-04-08 15:58:37 +0500

    adjust placeholder name

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                         <label class="floating-label mb-6">
                             <input type="email"
                                    name="email"
-                                   placeholder="[mail@example.com](<mailto:mail@example.com>)"
+                                   placeholder="saifi11@gmail.com"
                                    value="{{ old('email') }}"
                                    class="input input-bordered @error('email') input-error @enderror"
                                    required
