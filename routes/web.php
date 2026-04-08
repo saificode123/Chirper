d1dcaa49218bd9dc04ce2c60c85c1319f35dfe34
Saif Ur Rehman Siddiqui <urrehmansaif406@gmail.com>
2026-04-08 13:07:34 +0500

    correct edit update delete feature

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/',[ChirpController::class,'index'] );
 Route::post('/chirps',[ChirpController::class,'store']);
 Route::get('/chirps/{chirp}/edit',[ChirpController::class,'edit']);
 Route::put('/chirps/{chirp}',[ChirpController::class,'update']);
-Route::delete('/chirps/{chirp}',[ChirpController::class,'destory']);
+Route::delete('/chirps/{chirp}',[ChirpController::class,'destroy']);
 
 // Route::resource('/chirps',ChirpController::class)
 // ->only('store','update','edit','destory');
